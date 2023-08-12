71668e7b89f54c46c21fef69f6c10698e56b667d
Youssef M.Hanfy <youssefhanfy99@gmail.com>
2023-08-13 02:28:47 +0300

    first commit

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart'as http ;
+
+import '../models/weather_model.dart';
+class WeatherService{
+  String baseUrl='http://api.weatherapi.com/v1';
+  String apiKey ='9df6786a07c34532bac04438230406';
+  Future<WeatherModel> getWeather({required String cityName}) async {
+    Uri url =
+    Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=7');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 400) {
+      var data  = jsonDecode(response.body);
+      throw Exception(data['error']['message']);
+    }
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    WeatherModel weather = WeatherModel.fromJson(data);
+
+    return weather;
+  }
+}
